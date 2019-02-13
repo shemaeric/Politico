cba41368773a704fa2d6cba3c36277e0ad2217b4
shema <shemaeric01@gmail.com>
2019-02-14 00:46:48 +0200

    #163962792-update modules

--- server/server.js
+++ server/server.js
@@ -1,34 +1,34 @@
 import express from 'express';
-import Party from './src/usingJSObject/controllers/partyController';
-import Office from './src/usingJSObject/controllers/officeController';
+import Party from './controllers/partyController';
+import Office from './controllers/officeController';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/api/v1", (req, res) => {
-  res.json({ message: "Welcome to Politico API" });
+app.get('/api/v1', (req, res) => {
+  res.json({ message: 'Welcome to Politico API' });
 });
 
 // create a party
 app.post('/api/v1/parties', Party.createPart);
 // get all parties
 app.get('/api/v1/parties', Party.getParties);
-//get a specific party
+// get a specific party
 app.get('/api/v1/parties/:id', Party.getParty);
-//update a party
+// update a party
 app.patch('/api/v1/parties/:id', Party.updatingParty);
-//delete a party
+// delete a party
 app.delete('/api/v1/parties/:id', Party.deletingParty);
 // create office
 app.post('/api/v1/offices/', Office.createOf);
 // get All offices
 app.get('/api/v1/offices/', Office.getOffices);
-//get a specific office
+// get a specific office
 app.get('/api/v1/offices/:id', Office.getOffice);
-//update an Office
+// update an Office
 app.patch('/api/v1/offices/:id', Office.updatingOffice);
-//delete a office
+// delete a office
 app.delete('/api/v1/offices/:id', Office.deletingOffice);
 
 const port = process.env.PORT || 3000;
---
+++ server/tests/partyTest.js
@@ -0,0 +1,182 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server';
+import Party from '../models/partyModel';
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+// Test the Create Party EndPoint
+describe('Create a Politico Party', () => {
+  it('should not create a party without all contents filled', (done) => {
+    const data = {
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    chai.request(app)
+      .post('/api/v1/parties')
+      .send(data)
+      .set('content-type', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body.message).to.equal('name and HeadQuater Address could not be empty');
+        done();
+      });
+  });
+  it('it should create a party', (done) => {
+    const data = {
+      id: 1,
+      name: 'democrats',
+      hqAdress: 'Washngton DC',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+    chai.request(app)
+      .post('/api/v1/parties')
+      .send(data)
+      .set('content-type', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.data[0]).to.have.property('name');
+        expect(res.body.message).to.equal('Party Succefully Created');
+        done();
+      });
+  });
+});
+
+// test Get all parties endpoint
+describe('Get all Politico Parties', () => {
+  it('it should get all parties', (done) => {
+    chai.request(app)
+      .get('/api/v1/parties')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('Object');
+        done();
+      });
+  });
+});
+
+describe('Get a specific Politico Parties', () => {
+  it('it should fail to get a specific party', (done) => {
+    const data = {
+      name: 'hie',
+      hqAdress: 'kigali',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    const party = Party.createParty(data);
+    chai.request(app)
+      .get('/api/v1/parties/dhfdafd')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('party not found');
+        done();
+      });
+  });
+
+  it('it should get a specific party', (done) => {
+    const data = {
+      name: 'hie',
+      hqAdress: 'kigali',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    const party = Party.createParty(data);
+    chai.request(app)
+      .get(`/api/v1/parties/${party.id}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('Object');
+        done();
+      });
+  });
+});
+
+describe('Update a Political Party', () => {
+  it('it should fail to Update a party', (done) => {
+    const data = {
+      name: 'democrats',
+      hqAdress: 'kigali',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    const party = Party.createParty(data);
+    chai.request(app)
+      .patch('/api/v1/parties/dhfdafd')
+      .send({ name: 'demo' })
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('party not found');
+        done();
+      });
+  });
+
+  it('it should update a party', (done) => {
+    const data = {
+      name: 'democrats',
+      hqAdress: 'kigali',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    const party = Party.createParty(data);
+    chai.request(app)
+      .patch(`/api/v1/parties/${party.id}`)
+      .send({ name: 'demo' })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('Object');
+        done();
+      });
+  });
+});
+
+describe('Delete a Political Party', () => {
+  it('it should fail to Delete a party', (done) => {
+    const data = {
+      name: 'democrats',
+      hqAdress: 'kigali',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    const party = Party.createParty(data);
+    chai.request(app)
+      .delete('/api/v1/parties/dhfdafd')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('Party not found');
+        done();
+      });
+  });
+
+  it('it should delete a party', (done) => {
+    const data = {
+      name: 'democrats',
+      hqAdress: 'kigali',
+      logoUrl: 'hiensisss',
+      createdDate: 23456,
+      modifiedDate: 2345,
+    };
+
+    const party = Party.createParty(data);
+    chai.request(app)
+      .delete(`/api/v1/parties/${party.id}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
