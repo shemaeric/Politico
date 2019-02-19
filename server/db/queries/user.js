c5de1ec58bae5b383b5441e5d33dc27725e9a1f8
shema <shemaeric01@gmail.com>
2019-02-19 05:35:06 +0200

    fix queries

--- server/db/queries/user.js
+++ server/db/queries/user.js
@@ -2,23 +2,23 @@ const createTableUsers = `
   CREATE TABLE IF NOT EXISTS
     users(
       id UUID PRIMARY KEY,
-      first_name VARCHAR(255) NOT NULL,
-      last_name VARCHAR(255) NOT NULL,
-      other_name VARCHAR(255),
+      firstName VARCHAR(255) NOT NULL,
+      lastName VARCHAR(255) NOT NULL,
+      otherName VARCHAR(255),
       email VARCHAR(255) NOT NULL UNIQUE,     
       phone VARCHAR(255),
       passport VARCHAR(255),
       password VARCHAR(255),
       createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 
     )`;
 
 const insertUser = `INSERT INTO users(
   id,
-  first_name,
-  last_name,
-  other_name,
+  firstName,
+  lastName,
+  otherName,
   email,
   phone,
   passport,
@@ -32,5 +32,5 @@ const dropTableUsers = 'DROP TABLE IF EXISTS users';
 export default {
   createTableUsers,
   insertUser,
-  dropTableUsers,
+  dropTableUsers
 };
